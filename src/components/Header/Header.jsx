f4680b58fce738d021d50f3ca16ca48256689d3f
boileroo <boileroo88@googlemail.com>
2022-04-09 11:12:02 +0100

    Add logo to header and fix reload button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import LABEL from "../../constants/Labels";
+import CONSTANTS from "../../constants/Config";
 import { Button } from "../../components/Button";
 import { useLaunchContext } from "../../contexts/LaunchContext/LaunchContext";
 
 export const Header = () => {
-  const { filter } = useLaunchContext();
+  const { listLaunches } = useLaunchContext();
   return (
     <header className="app__header">
       <div className="app__logo">
-        {/* missing src  */}
-        <img className="app__logo-image" alt="" />
+        <img className="app__logo-image" alt="" src={CONSTANTS.LOGO} />
         <span className="app__logo-txt">{LABEL.LAUNCHES}</span>
       </div>
-      <Button
-        filter={filter}
-        //missing onClick
-        classes="button button--reload"
-        label={LABEL.RELOAD}
-        allowDisabledState={false}
-      />
+      <Button classes="button button--reload" label={LABEL.RELOAD} onClick={listLaunches} />
     </header>
   );
 };
