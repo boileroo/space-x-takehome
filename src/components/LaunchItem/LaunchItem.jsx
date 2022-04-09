c2f1b981b5c92b5e9b364aef7446af73a65d5735
boileroo <boileroo88@googlemail.com>
2022-04-09 11:06:42 +0100

    Format LaunchItem date and add rocket name

--- src/components/LaunchItem/LaunchItem.jsx
+++ src/components/LaunchItem/LaunchItem.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import moment from "moment";
 
 export const LaunchItem = ({ launch, index }) => {
-  //missing rocket name
-  const { flight_number, mission_name, launch_date_utc } = launch;
+  const { flight_number, mission_name, launch_date_utc, rocket } = launch;
+
+  let formattedLaunchDate = moment(launch_date_utc).format("Do MMM YYYY");
 
   return (
     <li key={index}>
       <div>
-        <span>{`#${flight_number}`}</span>
-        <span>{`${mission_name}`}</span>
+        <span>{`#${flight_number} `}</span>
+        <span>{`${mission_name} `}</span>
       </div>
       <div>
         <span>
-          {/* use moment here */}
-          <span>{launch_date_utc}</span>
-          {/* missing rocket name */}
+          <span>{`${formattedLaunchDate} `}</span>
+          <span>{`${rocket.rocket_name}`}</span>
         </span>
       </div>
     </li>
