6db7c046006868c5feabcd43eeab66a7dcdc6b28
boileroo <boileroo88@googlemail.com>
2022-04-09 20:07:54 +0100

    Add style to launch items

--- src/components/LaunchItem/LaunchItem.jsx
+++ src/components/LaunchItem/LaunchItem.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import moment from "moment";
 
-export const LaunchItem = ({ launch, index }) => {
+export const LaunchItem = ({ launch }) => {
   const { flight_number, mission_name, launch_date_utc, rocket } = launch;
 
   let formattedLaunchDate = moment(launch_date_utc).format("Do MMM YYYY");
 
   return (
-    <li key={index}>
-      <div>
+    <div className="launch-item">
+      <div className="launch-item__flight-number">
         <span>{`#${flight_number} `}</span>
+      </div>
+      <div className="launch-item__mission-name">
         <span>{`${mission_name} `}</span>
       </div>
-      <div>
+      <div className="launch-item__info">
         <span>
-          <span>{`${formattedLaunchDate} `}</span>
-          <span>{`${rocket.rocket_name}`}</span>
+          <div className="launch-item__info__date">{`${formattedLaunchDate} `}</div>
+          <div className="launch-item__info__rocket">{`${rocket.rocket_name}`}</div>
         </span>
       </div>
-    </li>
+    </div>
   );
 };
